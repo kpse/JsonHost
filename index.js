030761fbf5f73c66c3237b225f5def3c9961af26
Suo Qin <kpsesuo@gmail.com>
2014-08-01 17:51:45 +0800

    listening on 80 port

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var http = require('http');
 http.createServer(function (req, res) {
   var fileJSON = require('./facts.json');
   res.end(JSON.stringify(fileJSON));
-}).listen(1337, '127.0.0.1');
+}).listen(80, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/');
