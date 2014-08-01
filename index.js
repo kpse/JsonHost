e84798c65ec62d742912bcf3cf18de97a3424252
Suo Qin <kpsesuo@gmail.com>
2014-08-01 22:11:46 +0800

    add long version of json

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
 var http = require('http');
+var url = require("url");
+var fs = require("fs");
 var app = http.createServer(function (req, res) {
-  var fileJSON = require('./facts.json');
-  res.end(JSON.stringify(fileJSON));
+  var pathname = url.parse(req.url).pathname;
+  console.log(pathname);
+  if (/\/(\w+)\.json/.test(pathname)) {
+    try {
+      if (fs.existsSync('.' + pathname)) {
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        var fileJSON = require('.' + pathname);
+        res.end(JSON.stringify(fileJSON));
+      }
+      else {
+        defaultHandler(res)
+      }
+    } catch (e) {
+      defaultHandler(res)
+    }
+  }
+  else {
+    defaultHandler(res)
+  }
 });
+
+function defaultHandler(res) {
+  res.statusCode = 403;
+  res.end('');
+}
 var port = Number(process.env.PORT || 5000);
-app.listen(port, function() {
+app.listen(port, function () {
   console.log("Listening on " + port);
 });
