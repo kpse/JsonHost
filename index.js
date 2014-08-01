0a98e9d48c8803cc63a2ce306bd98066c764617e
Suo Qin <kpsesuo@gmail.com>
2014-08-01 17:07:30 +0800

    serve static json file

---
+++ index.js
@@ -0,0 +1,6 @@
+var http = require('http');
+http.createServer(function (req, res) {
+  var fileJSON = require('./facts.json');
+  res.end(JSON.stringify(fileJSON));
+}).listen(1337, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:1337/');
