bd10d81f0bf0260d61b0559c08dbf3f9127fc23a
Suo Qin <kpsesuo@gmail.com>
2014-08-05 21:19:43 +0800

    add unstable route

--- index.js
+++ index.js
@@ -3,17 +3,28 @@ var app = express();
 
 app.set('json spaces', 2);
 
-app.get('/facts.json', function(req, res){
+app.get('/facts.json', function (req, res) {
   var fileJSON = require('./json/facts.json');
   res.json(fileJSON);
 });
 
-app.get('/long_facts.json', function(req, res){
+app.get('/long_facts.json', function (req, res) {
   var fileJSON = require('./json/long_facts.json');
   res.json(fileJSON);
 });
 
-app.all('/*', function(req, res){
+app.get('/unstable_facts.json', function (req, res) {
+  setTimeout()
+  if (Math.random() > 0.5) {
+    res.status(500).send('error!');
+  } else {
+    var fileJSON = require('./json/facts.json');
+    res.json(fileJSON);
+  }
+
+});
+
+app.all('/*', function (req, res) {
   res.status(404).send('Sorry, we cannot find that!');
 });
 
